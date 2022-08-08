0505558b7378100553f334430d912ef5dd957156
Anup Mahato <anupmahato033@gmail.com>
2022-08-08 17:34:50 +0530

    added swagger in prod

--- Jira.Ms/Program.cs
+++ Jira.Ms/Program.cs
@@ -34,9 +34,9 @@ var app = builder.Build();
 /// add middlewares
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseDefaultFiles();
